ecf8eb47274a444578c63900c231a150c41f2979
AngelinaRS <candy26y_@hotmail.com>
2016-01-19 10:35:30 -0600

    Add file

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 // Javascript Code.
 //My key c84d8da47add8cb0
+
+$(document).ready(function() {
+
+	$("#SearchWeather").click(function(){
+		var country = $("#country").val();
+		var state = $("#state").val();
+		var typeTemperature = $("input[name=temperature]:checked").val();
+
+		if(country != "" && state != ""){
+
+			$.ajax({
+				url : "http://api.wunderground.com/api/c84d8da47add8cb0/conditions/q/" + country + "/" + state + ".json",
+				data : "jsonp",
+
+				success : function(json_parsed){
+					var city = json_parsed["current_observation"]["display_location"]["city"];
+					$("#nameCity").html(city);
+				}
+			});
+		} else {
+			alert("Insert Country and State")
+		}
+	});
+});
