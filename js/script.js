3d12ccb3818ba257a3a6184dff314ffd382cddc8
AngelinaRS <candy26y_@hotmail.com>
2016-01-20 12:52:18 -0600

    Already works

--- js/script.js
+++ js/script.js
@@ -15,12 +15,45 @@ $(document).ready(function() {
 				data : "jsonp",
 
 				success : function(json_parsed){
-					var city = json_parsed["current_observation"]["display_location"]["city"];
+					var city = json_parsed["current_observation"]["display_location"]["full"];
+					var weather = json_parsed["current_observation"]["weather"];
+					var icon = json_parsed["current_observation"]["icon_url"];
+					var wind = json_parsed["current_observation"]["wind_kph"];
+					var temp_f = json_parsed["current_observation"]["temp_f"];
+					var temp_c = json_parsed["current_observation"]["temp_c"];
+					var humidity = json_parsed["current_observation"]["relative_humidity"];
+					var latitude = json_parsed["current_observation"]["display_location"]["latitude"];
+					var longitude = json_parsed["current_observation"]["display_location"]["longitude"];
+					var elevation = json_parsed["current_observation"]["observation_location"]["elevation"];
+					$(".to-hide").addClass("hide");
 					$("#nameCity").html(city);
+					$("#weather").html(weather);
+					$("#icon").html("<img src" + "=" + "'" + icon + "'" + "/>");
+					$(".black").removeClass("hide");
+					$("#wind").html(wind + " kph");
+					$("#humidity").html(humidity);
+					$("#latitude").html(latitude);
+					$("#longitude").html(longitude);
+					$("#elevation").html(elevation);
+					$("#date").html(new Date());
+					$("#new-search").removeClass("hide");
+					if(typeTemperature === "fahrenheit"){
+						$("#temperature").html(temp_f + " °F");
+					} else {
+						$("#temperature").html(temp_c + " °C");
+					}
 				}
 			});
 		} else {
 			alert("Insert Country and State")
 		}
+	});
+
+	$("#new-search").click(function(){
+		document.getElementById("input-reset").reset();
+		location.reload(true);
+		$(".to-hide").removeClass("hide");
+		$(".to-show").addClass("hide");
+
 	});
 });
