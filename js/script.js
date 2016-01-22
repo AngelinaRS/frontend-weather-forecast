56a067c448387a0c3dfe4a91bb9e4e525292b102
AngelinaRS <candy26y_@hotmail.com>
2016-01-22 09:56:01 -0600

    Documented

--- js/script.js
+++ js/script.js
@@ -4,23 +4,26 @@
 $(document).ready(function() {
 
 	$("#SearchWeather").click(function(){
+		/*When the user click in the button [Search]
+		It obtains the value of the country, state and temperature (celsius or fahrenheit)*/
 		var country = $("#country").val();
 		var state = $("#state").val();
 		var typeTemperature = $("input[name=temperature]:checked").val();
 
-		if(country != "" && state != ""){
+		if(country != "" && state != ""){ /*Only if the inputs are not empty*/
 
 			$.ajax({
 				url : "http://api.wunderground.com/api/c84d8da47add8cb0/conditions/q/" + country + "/" + state + ".json",
 				data : "jsonp",
 
 				success : function(json_parsed){
-					try {
+					try { /*Try if the names are valids to search the weather forecast*/
 						var city = json_parsed["current_observation"]["display_location"]["full"];
 					} catch(error) {
 						alert("Write the correct names...");
-						location.reload();
+						location.reload(); /*If the names are invalids reload the page*/
 					}
+					/*it obtains data of the API json*/
 					var weather = json_parsed["current_observation"]["weather"];
 					var icon = json_parsed["current_observation"]["icon_url"];
 					var wind = json_parsed["current_observation"]["wind_kph"];
@@ -30,7 +33,7 @@ $(document).ready(function() {
 					var latitude = json_parsed["current_observation"]["display_location"]["latitude"];
 					var longitude = json_parsed["current_observation"]["display_location"]["longitude"];
 					var elevation = json_parsed["current_observation"]["observation_location"]["elevation"];
-					$(".to-hide").addClass("hide");
+					$(".to-hide").addClass("hide"); /*Hide some the inputs and button search*/
 					$("#nameCity").html(city);
 					$("#weather").html(weather);
 					$("#icon").html("<img src" + "=" + "'" + icon + "'" + "/>");
@@ -55,9 +58,10 @@ $(document).ready(function() {
 	});
 
 	$("#new-search").click(function(){
-		location.reload();
-		$(".to-hide").removeClass("hide");
-		$(".to-show").addClass("hide");
+		/*When the user click the button [new search]*/
+		location.reload(); /*Reload the page*/
+		$(".to-hide").removeClass("hide"); /*Show the inputs and button [Search]*/
+		$(".to-show").addClass("hide"); /*Hide the information of the weather*/
 
 	});
 });
